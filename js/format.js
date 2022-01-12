6573b8d0a7f1e37c618188c99d4372975f2d08a7
Darcy Merilan <darcymer@byui.edu>
2022-01-11 17:40:31 -0700

    final touches to make narrow screen's adaptable

--- js/format.js
+++ js/format.js
@@ -4,11 +4,11 @@ let year = date.getFullYear();
 
 console.log(year); //or (for the full date)
 
-document.querySelector('.Copywright').innerHTML = "<p> &copy" + year + " .:|:. Darcy Merilan .:|:. Idaho</p>";
+document.querySelector('.copywright').innerHTML = "<p> &copy" + year + " .:|:. Darcy Merilan .:|:. Idaho</p>";
 // another way.... document.queryselector(#copyrightyear).innerText = new Date().getFullyear();
 //let Modif = new Date(document.lastModified)
 
-document.querySelector('.Timing').innerHTML = "<p> Last Update: " + document.lastModified + " </p>";
+document.querySelector('.timing').innerHTML = "<p> Last Update: " + document.lastModified + " </p>";
 // another way.... document.getElementbyID("lastmodifiedDate").innerText = document.lastModified;
 
 //innertext is faster than innerHtml
