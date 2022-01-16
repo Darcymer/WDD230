52088e98d2efd32b677761f45177eb71e87655a0
Darcy Merilan <darcymer@byui.edu>
2022-01-15 17:06:59 -0700

    fixed footer and linked images

--- Lesson 02/js/desprin.js
+++ Lesson 02/js/desprin.js
@@ -3,12 +3,11 @@ let year = date.getFullYear();
 
 console.log(year); //or (for the full date)
 
-document.querySelector('.copywright').innerHTML = "<p> &copy" + year + " .:|:. Darcy Merilan .:|:. Idaho</p>";
+document.querySelector('.copywright').innerHTML = "<p> &copy" + year + " | Darcy Merilan | Last Update: " + document.lastModified + " </p>";
 //another way....
 //document.queryselector(#copywright).innerText = new Date().getFullyear();
 //let Modif = new Date(document.lastModified)
 
-document.querySelector('.timing').innerHTML = "<p> Last Update: " + document.lastModified + " </p>";
 // another way....  document.getElementbyID("lastmodifiedDate").innerText = document.lastModified;
 
 //innertext is faster than innerHtml
