8d44870653a8c81b362980c5337f1d1b3e87404f
Darcy Merilan <darcymer@byui.edu>
2022-01-22 19:58:44 -0700

    lesson 3 commerce site begins

---
+++ lesson3/js/chamber.js
@@ -0,0 +1,14 @@
+let date = new Date();
+let year = date.getFullYear();
+
+//console.log(year); //or (for the full date)
+
+document.querySelector('.copywright').innerHTML = "<p> &copy" + year + " | Darcy Merilan | Last Update: " + document.lastModified + "</p>";
+//another way....
+//document.queryselector(#copywright).innerText = new Date().getFullyear();
+//let Modif = new Date(document.lastModified)
+
+// another way....  document.getElementbyID("lastmodifiedDate").innerText = document.lastModified;
+
+//innertext is faster than innerHtml
+//with Javascript try to use id when manipulating html, and not classes
